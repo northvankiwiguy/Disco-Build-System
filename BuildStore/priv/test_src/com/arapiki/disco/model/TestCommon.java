5f5a1ceaa1b45ad6ed2e90c140a8205f0c56383c
Peter Smith <psmith@arapiki.com>
2011-01-31 08:29:20 -0800

    Add the sortedArraysEqual() method for comparing the content of two arrays, regardless of the order of elements.

--- BuildStore/priv/test_src/com/arapiki/disco/model/TestCommon.java
+++ BuildStore/priv/test_src/com/arapiki/disco/model/TestCommon.java
@@ -12,6 +12,8 @@
 
 package com.arapiki.disco.model;
 
+import java.util.Arrays;
+
 
 /**
  * Common methods for testing BuildStore and related classes.
@@ -35,4 +37,31 @@ public class TestCommon {
 		
 		return bs;
 	}	
+	
+	/* 
+	 * Determine whether two arrays contain the same elements, even if those elements
+	 * aren't in the same order in both arrays.
+	 */
+	public static boolean sortedArraysEqual(Object[] arr1, Object[] arr2) {
+		
+		/* if one is null, then both must be null */
+		if (arr1 == null) {
+			return (arr2 == null);
+		}
+		
+		/* types must be the same */
+		if (arr1.getClass() != arr2.getClass()){
+			return false;
+		}
+
+		/* lengths must be the same */
+		if (arr1.length != arr2.length) {
+			return false;
+		}
+		
+		/* now sort the elements and compare them */
+		Arrays.sort(arr1);
+		Arrays.sort(arr2);
+		return Arrays.equals(arr1, arr2);		
+	}
 }
