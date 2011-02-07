e2257f301f104596cd222581df6dc67c3d235f5f
Peter Smith <psmith@arapiki.com>
2011-02-07 09:00:24 -0800

    Initial commit of DiscoMain project, including the Apache Commons CLI library.

---
+++ DiscoMain/priv/src/com/arapiki/disco/main/DiscoMain.java
@@ -0,0 +1,31 @@
+/*******************************************************************************
+ * Copyright (c) 2011 Arapiki Solutions Inc.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    "Peter Smith <psmith@arapiki.com>" - initial API and 
+ *        implementation and/or initial documentation
+ *******************************************************************************/ 
+
+package com.arapiki.disco.main;
+
+/**
+ * This is the main entry point for the "disco" command line program. All other projects (with
+ * the exception of the Eclipse plug-in are invoked from this point.
+ * 
+ * @author "Peter Smith <psmith@arapiki.com>"
+ */
+public class DiscoMain {	
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
