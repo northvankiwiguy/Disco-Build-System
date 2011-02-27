9dbb0fe36fa3424871d7a703c6ab45db64964fd8
Peter Smith <psmith@arapiki.com>
2011-02-26 17:21:47 -0800

    Add a new Error class for throwing fatal errors - should be caught and handled by higher-level code.

---
+++ Utilities/priv/src/com/arapiki/utils/errors/FatalError.java
@@ -0,0 +1,42 @@
+/*******************************************************************************
+ * Copyright (c) 2011 Arapiki Solutions Inc.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    "Peter Smith <psmith@arapiki.com>" - initial API and 
+ *        implementation and/or initial documentation
+ *******************************************************************************/ 
+
+package com.arapiki.utils.errors;
+
+/**
+ * A Java Error that can be thrown from any part of the code
+ * to indicate that a fatal error occurred. The receiver of the FatalError
+ * must simply catch and display the error. There isn't intended to be any way to
+ * recovery from this type of error.
+ * @author "Peter Smith <psmith@arapiki.com>"
+ */
+@SuppressWarnings("serial")
+public class FatalError extends Error {
+	
+	/**
+	 * Create a new FataError
+	 * @param message A string message to indicate the cause of the failure.
+	 * @param e An embedded Exception that caused the failure.
+	 */
+	public FatalError(String message, Throwable e) {
+		super(message, e);
+	}
+	
+	/**
+	 * Create a new FataError.
+	 * @param message A string message to indicate the cause of the failure.
+	 */
+	public FatalError(String message) {
+		super(message);
+	}
+
+}
