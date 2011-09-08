e2f816d1a3ad360d0a31399cdf85e425abbbb06f
Peter Smith <psmith@arapiki.com>
2011-09-07 20:42:46 -0700

    Add an (empty) file that's required by the build, but has never been committed before. This file is a work in progress.

---
+++ ComponentFS/priv/src/interposer/file_name_utils.c
@@ -0,0 +1,17 @@
+/*******************************************************************************
+ * Copyright (c) 2010 Arapiki Solutions Inc.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    "Peter Smith <psmith@arapiki.com>" - initial API and 
+ *        implementation and/or initial documentation
+ *******************************************************************************/ 
+
+/*
+ * This file contains functions for normalizing file names, as well as filtering
+ * out files that we don't care to monitor.
+ */
+
