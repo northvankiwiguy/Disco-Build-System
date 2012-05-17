5e9ab217191188132c422db0bb61e6ea23a1e848
Peter Smith <psmith@arapiki.com>
2012-05-16 19:29:40 -0700

    Add a toolbar menu to allow creation of a new editor tab. Not only can we duplicate the current tab, but we can select an empty tab, a full tab, all unused files, all write-only files, etc.

--- EclipsePlugin/src/com/arapiki/disco/eclipse/files/handlers/HandlerNewEditorTab.java
+++ EclipsePlugin/src/com/arapiki/disco/eclipse/files/handlers/HandlerNewEditorTab.java
@@ -3,14 +3,18 @@ package com.arapiki.disco.eclipse.files.handlers;
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.swt.widgets.Display;
 import org.eclipse.ui.IEditorPart;
 import org.eclipse.ui.handlers.HandlerUtil;
+import org.eclipse.ui.part.EditorPart;
 
 import com.arapiki.disco.eclipse.DiscoMainEditor;
 import com.arapiki.disco.eclipse.files.DiscoFilesEditor;
 import com.arapiki.disco.eclipse.tasks.DiscoTasksEditor;
+import com.arapiki.disco.eclipse.utils.EclipsePartUtils;
 import com.arapiki.disco.eclipse.utils.errors.FatalDiscoError;
 import com.arapiki.disco.model.BuildStore;
+import com.arapiki.disco.model.Reports;
 import com.arapiki.disco.model.types.ComponentSet;
 import com.arapiki.disco.model.types.FileSet;
 
@@ -32,42 +36,97 @@ public class HandlerNewEditorTab extends AbstractHandler {
 		/* fetch the active editor, and its BuildStore, and the active sub-editor. */
 		DiscoMainEditor mainEditor = (DiscoMainEditor)HandlerUtil.getActiveEditor(event);
 		BuildStore buildStore = mainEditor.getBuildStore();
-		IEditorPart subEditor = mainEditor.getActiveSubEditor();
+		Reports reports = buildStore.getReports();
+		IEditorPart currentEditor = mainEditor.getActiveSubEditor();
+		EditorPart newEditor = null;
+		
+		/* 
+		 * Determine exactly which (sub)command should be executed. If this is null, then the user
+		 * would have pressed the top-level toolbar button, which doesn't have a parameter attached.
+		 */
+		String subCmd = event.getParameter("com.arapiki.disco.eclipse.commandParameters.newEditorTab");
+		if (subCmd == null) {
+			subCmd = "duplicate";
+		}
 		
 		/*
-		 * Create a new DiscoFilesEditor?
+		 * Option 1: Duplicate the current editor tab.
 		 */
-		if (subEditor instanceof DiscoFilesEditor) {
-			DiscoFilesEditor existingEditor = (DiscoFilesEditor)subEditor;
-			
-			/* 
-			 * Create a new DiscoFilesEditor, and populate it with the same state as the
-			 * existing editor.
-			 */
-			DiscoFilesEditor newEditor = 
-				new DiscoFilesEditor(buildStore, existingEditor.getTitle() + "(new)");
-			newEditor.setOptions(existingEditor.getOptions());
-			try {
-				newEditor.setFilterComponentSet(
-						(ComponentSet)(existingEditor.getFilterComponentSet().clone()));
-				newEditor.setVisibilityFilterSet(
-						(FileSet)(existingEditor.getVisibilityFilterSet().clone()));
-			} catch (CloneNotSupportedException e) {
-				throw new FatalDiscoError("Unable to duplicate a DiscoFilesEditor");
+		if (subCmd.equals("duplicate")) {
+			if (currentEditor instanceof DiscoFilesEditor) {
+				DiscoFilesEditor existingEditor = (DiscoFilesEditor)currentEditor;
+				DiscoFilesEditor newFilesEditor = 
+					new DiscoFilesEditor(buildStore, existingEditor.getTitle() + " (copy)");
+				newFilesEditor.setOptions(existingEditor.getOptions());
+				try {
+					newFilesEditor.setFilterComponentSet(
+							(ComponentSet)(existingEditor.getFilterComponentSet().clone()));
+					newFilesEditor.setVisibilityFilterSet(
+							(FileSet)(existingEditor.getVisibilityFilterSet().clone()));
+				} catch (CloneNotSupportedException e) {
+					throw new FatalDiscoError("Unable to duplicate a DiscoFilesEditor");
+				}
+				newEditor = newFilesEditor;
+			}		
+			else if (currentEditor instanceof DiscoTasksEditor) {
+				// TODO: support DiscoTasksEditor.
 			}
-			
-			/* add the new editor as a new tab */
+		}
+		
+		/*
+		 * Option 2: New files editor, showing all files.
+		 */
+		else if (subCmd.equals("allFiles")) {
+			DiscoFilesEditor newFilesEditor = new DiscoFilesEditor(buildStore, "All Files");
+			newFilesEditor.setVisibilityFilterSet(reports.reportAllFiles());
+			newEditor = newFilesEditor;
+		}
+
+		/*
+		 * Option 3: New files editor, not showing any files.
+		 */
+		else if (subCmd.equals("emptyFiles")) {
+			DiscoFilesEditor newFilesEditor = new DiscoFilesEditor(buildStore, "Empty");
+			newFilesEditor.setVisibilityFilterSet(new FileSet(buildStore.getFileNameSpaces()));
+			newEditor = newFilesEditor;
+		}
+
+		/*
+		 * Option 4: New files editor, showing unused files.
+		 */
+		else if (subCmd.equals("unusedFiles")) {
+			DiscoFilesEditor newFilesEditor = new DiscoFilesEditor(buildStore, "Unused Files");
+			FileSet unusedFileSet = reports.reportFilesNeverAccessed();
+			unusedFileSet.populateWithParents();
+			newFilesEditor.setVisibilityFilterSet(unusedFileSet);
+			newEditor = newFilesEditor;
+		}
+
+		/*
+		 * Option 5: New files editor, showing write-only files.
+		 */
+		else if (subCmd.equals("writeOnlyFiles")) {
+			DiscoFilesEditor newFilesEditor = new DiscoFilesEditor(buildStore, "Write-only Files");
+			FileSet writeOnlyFileSet = reports.reportWriteOnlyFiles();
+			writeOnlyFileSet.populateWithParents();
+			newFilesEditor.setVisibilityFilterSet(writeOnlyFileSet);
+			newEditor = newFilesEditor;
+		}
+
+		/*
+		 * Option 6: Most popular files.
+		 */
+		else if (subCmd.equals("mostPopularFiles")) {
+			EclipsePartUtils.displayErrorDialog(Display.getDefault().getActiveShell(), "Not Implemented");
+		}
+		
+		/*
+		 * Finish by adding the new editor to the main editor, as a new tab.
+		 */
+		if (newEditor != null) {
 			mainEditor.newPage(newEditor);
 			mainEditor.setActiveEditor(newEditor);
 		}
-		
-		/*
-		 * Create a new DiscoTasksEditor?
-		 */
-		else if (subEditor instanceof DiscoTasksEditor) {
-			// TODO: support DiscoTasksEditor.
-		}
-		
 		return null;
 	}
 	
