e387511d470d62892c7ced04bcfe97fcad475cf3
Peter Smith <psmith@arapiki.com>
2012-08-11 16:57:40 -0700

    Modify the Alert methods so they're also run as the UI thread (allowing non-UI code to display an error).

--- EclipsePlugin/src/com/buildml/eclipse/utils/AlertDialog.java
+++ EclipsePlugin/src/com/buildml/eclipse/utils/AlertDialog.java
@@ -12,6 +12,9 @@
 
 package com.buildml.eclipse.utils;
 
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
 import org.eclipse.jface.dialogs.IDialogConstants;
 import org.eclipse.jface.dialogs.IMessageProvider;
 import org.eclipse.jface.dialogs.TitleAreaDialog;
@@ -22,6 +25,7 @@ import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Label;
+import org.eclipse.ui.progress.UIJob;
 
 /**
  * A general purpose dialog box for reporting information, warnings and errors to the 
@@ -45,7 +49,7 @@ public class AlertDialog extends TitleAreaDialog {
 	private int severity;
 	
 	/*=====================================================================================*
-	 * PUBLIC METHODS
+	 * CONSTRUCTORS
 	 *=====================================================================================*/
 	
 	/**
@@ -62,8 +66,51 @@ public class AlertDialog extends TitleAreaDialog {
 		this.severity = severity;
 	}
 	
+	/*=====================================================================================*
+	 * PUBLIC METHODS
+	 *=====================================================================================*/
+	
+	/**
+	 * Display an informational dialog box in the context of the current shell. Both
+	 * a general message and an explanatory reason will be display. Given that there's
+	 * only an "OK" button, there's no return code needed.
+	 * @param title The general informational message to be displayed.
+	 * @param message The detailed reason for the event.
+	 */
+	public static void displayInfoDialog(String title, String message) {
+		openDialog(title, message, IMessageProvider.INFORMATION);
+	}
+	
 	/*-------------------------------------------------------------------------------------*/
 
+	/**
+	 * Display an warning dialog box in the context of the current shell. Both
+	 * a general message and an explanatory reason will be display. Given that there's
+	 * only an "OK" button, there's no return code needed.
+	 * @param title The general informational message to be displayed.
+	 * @param message The detailed reason for the event.
+	 */
+	public static void displayWarningDialog(String title, String message) {
+		openDialog(title, message, IMessageProvider.WARNING);
+	}
+	
+	/*-------------------------------------------------------------------------------------*/
+	
+	/**
+	 * Display an error dialog box in the context of the current shell. Both
+	 * a general message and an explanatory reason will be display. Given that there's
+	 * only an "OK" button, there's no return code needed.
+	 * @param title The general error message to be displayed.
+	 * @param message The detailed reason for the event.
+	 */
+	public static void displayErrorDialog(final String title, final String message) {
+		openDialog(title, message, IMessageProvider.ERROR);
+	}
+	
+	/*=====================================================================================*
+	 * PROTECTED METHODS
+	 *=====================================================================================*/
+	
 	/* (non-Javadoc)
 	 * @see org.eclipse.jface.dialogs.Dialog#createDialogArea(org.eclipse.swt.widgets.Composite)
 	 */
@@ -82,7 +129,7 @@ public class AlertDialog extends TitleAreaDialog {
 		
 		/* create a label for the message, and centre it */
 		if (message != null) {
-			Label label = new Label(composite, 0);
+			Label label = new Label(composite, SWT.NONE);
 			GridData gridData = new GridData(SWT.FILL, SWT.FILL, true, true);
 			gridData.horizontalAlignment = SWT.CENTER;
 			gridData.verticalAlignment = SWT.CENTER;
@@ -112,46 +159,29 @@ public class AlertDialog extends TitleAreaDialog {
 		}
 	}
 	
-	/*-------------------------------------------------------------------------------------*/
-
-	/**
-	 * Display an informational dialog box in the context of the current shell. Both
-	 * a general message and an explanatory reason will be display. Given that there's
-	 * only an "OK" button, there's no return code needed.
-	 * @param title The general informational message to be displayed.
-	 * @param message The detailed reason for the event.
-	 */
-	public static void displayInfoDialog(String title, String message) {
-		AlertDialog dialog = new AlertDialog(title, message, IMessageProvider.INFORMATION);
-		dialog.open();
-	}
-	
-	/*-------------------------------------------------------------------------------------*/
-
-	/**
-	 * Display an warning dialog box in the context of the current shell. Both
-	 * a general message and an explanatory reason will be display. Given that there's
-	 * only an "OK" button, there's no return code needed.
-	 * @param title The general informational message to be displayed.
-	 * @param message The detailed reason for the event.
-	 */
-	public static void displayWarningDialog(String title, String message) {
-		AlertDialog dialog = new AlertDialog(title, message, IMessageProvider.WARNING);
-		dialog.open();
-	}
-	
-	/*-------------------------------------------------------------------------------------*/
+	/*=====================================================================================*
+	 * PRIVATE METHODS
+	 *=====================================================================================*/
 	
 	/**
-	 * Display an error dialog box in the context of the current shell. Both
-	 * a general message and an explanatory reason will be display. Given that there's
-	 * only an "OK" button, there's no return code needed.
-	 * @param title The general error message to be displayed.
-	 * @param message The detailed reason for the event.
+	 * Create and open a dialog box, using the UI thread. This allows non-UI code to 
+	 * raise an error.
+	 * 
+	 * @param title The title to display in the dialog box.
+	 * @param message The detailed message to display in the dialog box.
+	 * @param severity The severity level (e.g. IMessageProvider.ERROR).
 	 */
-	public static void displayErrorDialog(String title, String message) {
-		AlertDialog dialog = new AlertDialog(title, message, IMessageProvider.ERROR);
-		dialog.open();
+	private static void openDialog(final String title, final String message, final int severity) {
+		UIJob dialogJob = new UIJob("Dialog") {
+			@Override
+			public IStatus runInUIThread(IProgressMonitor monitor) {
+				AlertDialog dialog = new AlertDialog(title, message, severity);
+				dialog.open();
+				return Status.OK_STATUS;
+			}
+		};
+		dialogJob.setSystem(true);
+		dialogJob.schedule();
 	}
 	
 	/*-------------------------------------------------------------------------------------*/
