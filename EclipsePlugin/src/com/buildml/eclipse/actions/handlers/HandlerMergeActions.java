8a2f5d92d8b98d3b52419f95d3b8a2a970383a76
Peter Smith <psmith@arapiki.com>
2014-01-14 04:23:02 -0800

    Implement the "Merge Actions Into Groups" function, and the associate dialog for allowing the user to divide a set of actions into sub-groups.

--- EclipsePlugin/src/com/buildml/eclipse/actions/handlers/HandlerMergeActions.java
+++ EclipsePlugin/src/com/buildml/eclipse/actions/handlers/HandlerMergeActions.java
@@ -1,5 +1,9 @@
 package com.buildml.eclipse.actions.handlers;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
@@ -7,10 +11,13 @@ import org.eclipse.jface.viewers.TreeSelection;
 import org.eclipse.ui.handlers.HandlerUtil;
 
 import com.buildml.eclipse.MainEditor;
+import com.buildml.eclipse.bobj.UIAction;
 import com.buildml.eclipse.utils.AlertDialog;
 import com.buildml.eclipse.utils.ConversionUtils;
 import com.buildml.eclipse.utils.EclipsePartUtils;
 import com.buildml.eclipse.utils.UndoOpAdapter;
+import com.buildml.eclipse.utils.dialogs.SubGroupSelectionDialog;
+import com.buildml.eclipse.utils.errors.FatalError;
 import com.buildml.model.IActionMgr;
 import com.buildml.model.IBuildStore;
 import com.buildml.model.types.ActionSet;
@@ -39,24 +46,63 @@ public class HandlerMergeActions extends AbstractHandler {
 
 		MainEditor mainEditor = EclipsePartUtils.getActiveMainEditor();
 		IBuildStore buildStore = mainEditor.getBuildStore();
-		IActionMgr actionMgr = buildStore.getActionMgr();
 		IImportRefactorer refactorer = mainEditor.getImportRefactorer();
-
+		TreeSelection selection = (TreeSelection)HandlerUtil.getCurrentSelection(event);
 		String mode = event.getParameter("com.buildml.eclipse.commandParameters.mergeAction");
-		if (!mode.equals("selected")) {
-			System.out.println("Mode: " + mode);
-			return null;
+
+		/*
+		 * In "selected" mode, we merge together all of the selected actions (into a single
+		 * action).
+		 */
+		if (mode.equals("selected")) {
+			
+			/* build an ActionSet of all the selected actions */
+			ActionSet selectedActions = EclipsePartUtils.getActionSetFromSelection(buildStore, selection);
+			MultiUndoOp multiOp = new MultiUndoOp();
+			performMergeActions(refactorer, buildStore, selectedActions, multiOp);
+			new UndoOpAdapter("Merge Actions", multiOp).invoke();
 		}
 		
-		/* build an ActionSet of all the selected actions */
-		TreeSelection selection = (TreeSelection)HandlerUtil.getCurrentSelection(event);
-		ActionSet selectedActions = EclipsePartUtils.getActionSetFromSelection(buildStore, selection);
+		/*
+		 * In "groups" mode, we provide a dialog box where the user can select the size of
+		 * each sub-group. We then proceed to merge those subgroups.
+		 */
+		else if (mode.equals("groups")) {
+			MultiUndoOp multiOp = new MultiUndoOp();
+			performGroupMerges(refactorer, buildStore, selection, multiOp);
+			new UndoOpAdapter("Merge Actions Into Groups", multiOp).invoke();
+		}
+		
+		/* else, not supported - throw internal error */
+		else {
+			throw new FatalError("Unsupported mode in com.buildml.eclipse.commandParameters.mergeAction");
+		}
+		
+		return null;
+	}
+	
+	/*=====================================================================================*
+	 * PRIVATE METHODS
+	 *=====================================================================================*/
+
+	/**
+	 * Perform the merge action operation, with appropriate sanity tests, error reporting,
+	 * and scheduling of a MultiUndoOp. This method will merge all specified actions into
+	 * a single action.
+	 * 
+	 * @param refactorer		The IImportRefactorer to do the merging work.
+	 * @param buildStore		The IBuildStore that stores the actions.
+	 * @param selectedActions	The actions to be merged.
+	 * @param multiOp			The MultiUndoOp that all operations will be added to.
+	 */
+	private void performMergeActions(IImportRefactorer refactorer,
+			IBuildStore buildStore, ActionSet selectedActions, MultiUndoOp multiOp) {
+		
+		IActionMgr actionMgr = buildStore.getActionMgr();
 		
 		/* attempt to perform the "merge action" operation */
 		try {
-			MultiUndoOp multiOp = new MultiUndoOp();
 			refactorer.mergeActions(multiOp, selectedActions);
-			new UndoOpAdapter("Merge Actions", multiOp).invoke();
 			
 		} catch (CanNotRefactorException e) {
 
@@ -77,8 +123,85 @@ public class HandlerMergeActions extends AbstractHandler {
 							e.getCauseCode());
 			}
 		}
+	}
+	
+	/*-------------------------------------------------------------------------------------*/
+
+	/**
+	 * Given the current selection, prompt the user to divide that selection into sub-groups,
+	 * with each sub-group being merged into an action.
+	 * 
+	 * @param refactorer	The IImportRefactorer to do the merging work.
+	 * @param buildStore 	The IBuildStore that contains the actions.
+	 * @param selection 	The selection set of tree elements.
+	 * @param multiOp       The undo/redo operation into which we schedule merges.
+	 * 
+	 */
+	private void performGroupMerges(IImportRefactorer refactorer, IBuildStore buildStore, 
+									TreeSelection selection, MultiUndoOp multiOp) {
 		
-		return null;
+		IActionMgr actionMgr = buildStore.getActionMgr();
+		
+		/*
+		 * Compute two ordered lists: 1) The selection action IDs, 2) The command strings
+		 * for those action IDs. The order is important, as it'll dictate which actions
+		 * are merged into which sub-groups.
+		 */
+		List<Integer> actionIDs = new ArrayList<Integer>(selection.size());
+		List<String> actionCmds = new ArrayList<String>(selection.size());
+		Iterator<Object> iter = selection.iterator();
+		while (iter.hasNext()) {
+			Object item = iter.next();
+			if (item instanceof UIAction) {
+				UIAction action = (UIAction)item;
+				int actionId = action.getId();
+				String cmdString = (String) actionMgr.getSlotValue(actionId, IActionMgr.COMMAND_SLOT_ID);
+				actionIDs.add(actionId);
+				actionCmds.add(cmdString);
+			}
+		}
+		
+		/*
+		 * Pass the list to a Dialog, to ask the user the size of the sub-group to use.
+		 */
+		SubGroupSelectionDialog dialog = 
+				new SubGroupSelectionDialog(actionCmds.toArray(new String[actionCmds.size()]));
+		int status = dialog.open();
+		if (status == SubGroupSelectionDialog.CANCEL) {
+			return;
+		}
+		
+		/*
+		 * Fetch the selected sub-group size (0 == no selection was made).
+		 */
+		int groupSize = dialog.getGroupSize();
+		if (groupSize == 0) {
+			return;
+		}
+		
+		/*
+		 * Now, perform the "merge action" operation on each subgroup. We create a new
+		 * ActionSet for each subgroup and use performMergeActions() to merge them each
+		 * into a single action.
+		 */
+		int size = actionIDs.size();
+		ActionSet actionsToMerge = null;
+		for (int i = 0; i != size; i++) {
+			
+			/* are we starting a new sub-group? */
+			if ((i % groupSize) == 0) {
+				actionsToMerge = new ActionSet(actionMgr);
+			}
+			
+			/* add the action ID to this sub group */
+			actionsToMerge.add(actionIDs.get(i));
+			
+			/* did we reach the end of the sub-group? */
+			if (((i+1) % groupSize) == 0) {
+				performMergeActions(refactorer, buildStore, actionsToMerge, multiOp);
+			}
+		}
+		
 	}
 	
 	/*-------------------------------------------------------------------------------------*/
---
+++ EclipsePlugin/src/com/buildml/eclipse/utils/dialogs/SubGroupSelectionDialog.java
@@ -0,0 +1,214 @@
+/*******************************************************************************
+ * Copyright (c) 2013 Arapiki Solutions Inc.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    psmith - initial API and 
+ *        implementation and/or initial documentation
+ *******************************************************************************/ 
+
+package com.buildml.eclipse.utils.dialogs;
+
+import java.util.ArrayList;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Combo;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.List;
+import org.eclipse.swt.widgets.Shell;
+import com.buildml.eclipse.utils.BmlTitleAreaDialog;
+
+/**
+ * A dialog allowing the user to select equal-sized subgroups of a list of items.
+ * This class can be used for any list of textual items, where there's a need to
+ * break those items into equal-sized subgroups.
+ * 
+ * @author Peter Smith <psmith@arapiki.com>
+ */
+public class SubGroupSelectionDialog extends BmlTitleAreaDialog {
+
+	/*=====================================================================================*
+	 * FIELDS/TYPES
+	 *=====================================================================================*/
+	
+	/** The items to be grouped */
+	private String[] items;
+
+	/** All the valid sub group sizes */
+	private ArrayList<Integer> validSizes;
+
+	/** The chosen subgroup size */
+	private int chosenSize = 0;
+	
+	/*=====================================================================================*
+	 * CONSTRUCTOR
+	 *=====================================================================================*/
+
+	/**
+	 * Create a new {@link SubGroupSelectionDialog}
+	 * 
+	 * @param items		The list of String items to be grouped.
+	 */
+	public SubGroupSelectionDialog(String [] items) {
+		super(new Shell());
+			
+		this.items = items;
+		
+		/* compute the possible sub-group sizes */
+		this.validSizes = computeDivisors(items.length);
+	}
+
+	/*=====================================================================================*
+	 * PUBLIC METHODS
+	 *=====================================================================================*/
+
+	/**
+	 * Return the chosen subgroup size. This method should only be called after the "OK"
+	 * button has been pressed.
+	 * 
+	 * @return The selected subgroup size, or 0 if no size was selected.
+	 */
+	public int getGroupSize() {
+		return chosenSize;
+	}
+	
+	/*=====================================================================================*
+	 * PROTECTED METHODS
+	 *=====================================================================================*/
+
+	/* (non-Javadoc)
+	 * @see com.buildml.eclipse.utils.BmlTitleAreaDialog#createDialogArea(org.eclipse.swt.widgets.Composite)
+	 */
+	@Override
+	protected Control createDialogArea(Composite parent) {
+				
+		/* Create the main panel widget for the body of the dialog box */
+		Composite panel = new Composite(parent, SWT.NONE);
+		panel.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
+		GridLayout layout = new GridLayout();
+		layout.marginHeight = 10;
+		layout.marginWidth = 20;
+		layout.verticalSpacing = 10;
+		layout.numColumns = 1;
+		panel.setLayout(layout);
+		
+		/*
+		 * Is the size of the input list appropriate? That is, are there any valid sub-group
+		 * sizes that we can merge them into?
+		 */
+		if (validSizes.size() == 0) {
+			setErrorMessage("Invalid number of items selected - can't divide into groups");
+			Label error = new Label(panel, SWT.WRAP);
+			error.setLayoutData(new GridData(SWT.FILL, SWT.NONE, true, false));
+			error.setText("To divide the selected items into smaller groups, " +
+					"you must select an equally-divisable number of items. For example, " +
+					"a multiple of 2, a multiple of 3, etc.");
+			return parent;
+		}
+				
+		/*
+		 * Informative message
+		 */
+		new Label(panel, SWT.WRAP).setText("Please select the number of items to appear " +
+				"in each sub-group:");
+		
+		/* 
+		 * display the valid sizes in a pull-down list, and select the first size choice
+		 * as the default (often this is "2").
+		 */
+		final Combo sizeSelector = new Combo(panel, SWT.READ_ONLY);
+		for (int subGroupSize : validSizes) {
+			sizeSelector.add("Group Size: " + subGroupSize);
+		}
+		sizeSelector.select(0);
+		chosenSize = validSizes.get(0);
+		
+		/* 
+		 * Display all the strings in a list box, with spaces between each sub-group
+		 */
+		final List itemList = new List(panel, SWT.V_SCROLL | SWT.H_SCROLL | SWT.BORDER);
+		itemList.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
+		fillListBox(itemList, items, chosenSize);
+		
+		/*
+		 * If the user selects a different group size, redraw the content of the 
+		 */
+		sizeSelector.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				chosenSize = validSizes.get(sizeSelector.getSelectionIndex());
+				fillListBox(itemList, items, chosenSize);
+			}
+		});
+		
+		return parent;
+	}
+	
+	/*-------------------------------------------------------------------------------------*/
+
+	/* 
+	 * (non-Javadoc)
+	 * @see org.eclipse.jface.window.Window#configureShell(org.eclipse.swt.widgets.Shell)
+	 */
+	@Override
+	protected void configureShell(Shell newShell) {
+		super.configureShell(newShell);
+		
+		newShell.setText("Choose the Sub-Group Size");
+	}	
+
+	/*=====================================================================================*
+	 * PRIVATE METHODS
+	 *=====================================================================================*/
+
+	/**
+	 * Given the size of the input group, compute all the possible sub-group sizes that
+	 * evenly divide into the totalSize. For example, if totalSize is 10, then the valid
+	 * sub-group sizes are [2, 5].
+	 *  
+	 * @param totalSize The size of the whole input group.
+	 * @return An array of valid sub-group sizes.
+	 */
+	private ArrayList<Integer> computeDivisors(int totalSize) {
+		
+		ArrayList<Integer> results = new ArrayList<Integer>();
+		for (int i = 2; i <= totalSize / 2; i++) {
+			if ((totalSize % i) == 0) {
+				results.add(i);
+			}
+		}
+		return results;
+	}
+
+	/*-------------------------------------------------------------------------------------*/
+
+	/**
+	 * Populate the List widget with the items to be divided into sub-groups. Based on the
+	 * specified sub-group size, insert a divider line.
+	 * 
+	 * @param itemList		List widget to display the items within.
+	 * @param items			The items to be displayed.
+	 * @param subGroupSize	The size of each sub-group (for displaying divider lines).
+	 */
+	private void fillListBox(List itemList, String[] items, Integer subGroupSize) {
+
+		itemList.removeAll();
+		for (int i = 0; i < items.length; i++) {
+			if ((i != 0) && (i % subGroupSize == 0)) {
+				itemList.add("");
+			}
+			itemList.add(items[i]);
+		}
+	}
+	
+	/*-------------------------------------------------------------------------------------*/
+}
